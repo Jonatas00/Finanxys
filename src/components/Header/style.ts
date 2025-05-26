534de64a0c3383870e49aebcf415d91a1ada7c61
Jonatas00 <djonatas31@gmail.com>
2025-05-26 19:15:43 -0300

    feat: update Header styles to enhance layout and typography

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,16 +1,23 @@
 import { colors } from "@/constants/colors";
 import { StyleSheet } from "react-native";
 
+
 export const styles = StyleSheet.create({
   container: {
-    marginTop: 16,
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 16,
   },
+  icon: {
+    position: "absolute",
+    left: 16,
+    fontSize: 36,
+  },
   title: {
-    fontSize: 24,
+    flex: 1,
+    textAlign: "center",
+    fontSize: 36,
     fontWeight: "bold",
-    color: colors.textPrimary,
+    color: colors.textPrimary
   },
 });
