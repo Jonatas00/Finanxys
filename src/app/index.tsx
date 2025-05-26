4d4c3f3a10f3945a3c176c61882e322cb61b75a2
Jonatas00 <djonatas31@gmail.com>
2025-05-26 18:19:03 -0300

    feat: update Index component to remove placeholder text and integrate Header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,20 @@
+import { Card } from "@/components/Card/Card";
+import { Header } from "@/components/Header/Header";
 import { colors } from "@/constants/colors";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={style.container}>
-      <Text style={style.text}>Começe adicionando seu primeiro gasto</Text>
+      <Header />
 
+      <Card />
     </View >
   );
 }
 
 const style = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
     flex: 1,
     backgroundColor: colors.background,
   },
