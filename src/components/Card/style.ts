82dfc090caaf5599ff83d9547b549d0162dc7c40
Jonatas00 <djonatas31@gmail.com>
2025-05-26 18:18:58 -0300

    feat: add Card component with styles

---
+++ src/components/Card/style.ts
@@ -0,0 +1,19 @@
+import { colors } from "@/constants/colors";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.buttonDefault,
+    height: 150,
+    margin: 16,
+    borderRadius: 8,
+  },
+  text: {
+    color: colors.textPrimary,
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 16,
+  },
+  description: {},
+
+})
