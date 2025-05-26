ef271802fb304f8d13d52552fb6d310b452617d2
Jonatas00 <djonatas31@gmail.com>
2025-05-26 18:18:34 -0300

    feat: refactor Layout component to use SafeAreaView and StatusBar; remove Stack component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,23 @@
-import { Stack } from "expo-router";
+import { colors } from "@/constants/colors";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false, statusBarStyle: "auto" }} />
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <StatusBar style="light" />
+        <Slot />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
