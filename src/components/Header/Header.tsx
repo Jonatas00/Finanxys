22d7f85714d6d4c52d9975e9bc11e29af0f29bb6
Jonatas00 <djonatas31@gmail.com>
2025-05-26 17:25:40 -0300

    feat: add Header component and styles; remove unused Button component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import Icon from 'react-native-vector-icons/Feather';
+
+export function Header() {
+  return (
+    <View>
+      <Icon name={"menu"}></Icon>
+    </View>
+  )
+}
