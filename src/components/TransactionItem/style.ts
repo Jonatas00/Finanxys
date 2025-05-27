4e9890e0228a88150e28ed1acfabe1fc2fc3dab4
Jonatas00 <djonatas31@gmail.com>
2025-05-27 20:53:07 -0300

    Update style for Transaction Item

--- src/components/TransactionItem/style.ts
+++ src/components/TransactionItem/style.ts
@@ -1,20 +1,18 @@
-import { colors } from "@/styles/colors";
+import { colors } from "@/utils/colors";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     backgroundColor: colors.buttonDefault,
     height: 80,
-    padding: 5,
+    paddingHorizontal: 8,
     marginVertical: 8,
     borderRadius: 8,
     boxShadow: colors.boxShadow,
-  },
-  left: {
-
-  },
-  right: {
-
   },
   title: {
     color: colors.textPrimary,
@@ -25,6 +23,7 @@ export const styles = StyleSheet.create({
     color: colors.textPrimary,
   },
   amount: {
+    fontSize: 20,
     color: colors.light
   }
 });
