69a4e34486d4911cf92b76ee9b02ef96cb8c830c
Jonatas00 <djonatas31@gmail.com>
2025-05-27 21:11:11 -0300

    fix: rearrange colors

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,17 +1,20 @@
 export const colors = {
   background: '#262626',
 
-  light: '#dcecac',
-  danger: '#fab4a7',
-  textTitle: '#7bae3e',
   textPrimary: '#f3f3f3',
   textSecondary: '#b6b6b6',
+  textTitle: '#7bae3e',
+
+  light: '#cafa91',
+  danger: '#ff9e9e',
+
   buttonText: '#ffffff',
   buttonDefault: '#7bae3e',
   buttonPressed: '#689f38',
   buttonDisabled: '#c9d7b0',
+
   modalPrimary: '#333333',
   modalSecondary: '#444444',
 
-  boxShadow: "2px 2px 3px rgba(0, 0, 0, 0.5)"
+  boxShadow: '2px 2px 3px rgba(0, 0, 0, 0.5)',
 };
