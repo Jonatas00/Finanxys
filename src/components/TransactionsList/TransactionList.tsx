993813d423e40f53d7a7c23f1d5f6ec2fa2a7bcb
Jonatas00 <djonatas31@gmail.com>
2025-05-27 22:35:07 -0300

    more tests...

--- src/components/TransactionsList/TransactionList.tsx
+++ src/components/TransactionsList/TransactionList.tsx
@@ -5,26 +5,25 @@ import { ScrollView, StyleSheet } from "react-native";
 const transactions: Transaction[] = [
   {
     id: 1,
-    title: "Transação 1",
+    title: "Transação teste 1",
     amount: 100,
     date: new Date(),
-    category: TransactionCategory.EXPENSE
+    category: TransactionCategory.INCOME
   },
   {
     id: 2,
-    title: "Transação 2",
+    title: "Transação teste 2",
     amount: 200,
     date: new Date(),
     category: TransactionCategory.EXPENSE
   },
   {
     id: 3,
-    title: "Salário",
-    amount: 3000000,
+    title: "Salário teste 3",
+    amount: 0,
     date: new Date(),
     category: TransactionCategory.INCOME
   }
-
 ]
 
 export default function TransactionList() {
