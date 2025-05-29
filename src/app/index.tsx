cea5de33a2139264a6e9d4a5cf4f867d1a8a407a
Jonatas <djonatas31@gmail.com>
2025-05-29 12:05:19 -0300

    organize imports

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,8 @@
 import Header from "@/components/Header/Header";
 import MainCard from "@/components/MainCard/MainCard";
 import TransactionList from "@/components/Transaction/TransactionList";
-import loadCustomFonts from "@/utils/loadCustomFonts";
 import { colors } from "@/utils/colors";
+import loadCustomFonts from "@/utils/loadCustomFonts";
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
