77432b58589dd0f1016105734c1c5c47d03ca0b6
Jonatas00 <djonatas31@gmail.com>
2025-05-29 21:00:48 -0300

    feat: use expo-crytpto to generate ids

--- src/components/Transaction/TransactionModal.tsx
+++ src/components/Transaction/TransactionModal.tsx
@@ -1,20 +1,21 @@
+import { AddTransactionButton } from "@/components/Button/AddTransactionButton";
 import { useModalStore } from "@/store/useModalStore";
 import { useTransactionStore } from "@/store/useTransactionStore";
 import { Transaction, TransactionCategory } from "@/types/transaction";
 import { colors } from "@/utils/colors";
 import { BlurView } from "expo-blur";
-import { Modal, StyleSheet, Text, TextInput, View } from "react-native";
+import * as Crypto from "expo-crypto";
+import { Modal, StyleSheet, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/Feather";
-
 
 export default function TransactionModal() {
   const { isModalOpen, closeModal } = useModalStore()
   const { addTransaction, clearTransactions } = useTransactionStore()
 
   const transaction: Transaction = {
-    id: Date.now(),
-    title: "primeiro teste com state",
-    amount: 50,
+    id: Crypto.randomUUID(),
+    title: "primeiro teste com modal",
+    amount: 500000,
     date: new Date(),
     category: TransactionCategory.INCOME
   }
@@ -29,19 +30,21 @@ export default function TransactionModal() {
         tint="dark"
         style={styles.centerView}
       >
+
         <View style={styles.modal}>
           <Text style={styles.title}>Adicionar Gasto</Text>
           <Icon style={styles.closeButton} name="x" onPress={closeModal}></Icon>
 
           <View style={{ gap: 50 }}>
-            <Text onPress={() => addTransaction(transaction)} style={{ color: colors.textPrimary }}>
-              CLIQUE AQUI PARA ADICIONAR GASTO!
-            </Text>
+
             <Text onPress={() => clearTransactions()} style={{ color: colors.textPrimary, backgroundColor: "green" }}>
               CLIQUE AQUI PARA REMOVER GASTOS!
             </Text>
+
+            <AddTransactionButton onPress={() => addTransaction(transaction)} />
           </View>
         </View>
+
       </BlurView>
     </Modal >
   )
