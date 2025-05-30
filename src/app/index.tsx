d63fa82af0592583972ef13d859576072f0c4764
Jonatas00 <djonatas31@gmail.com>
2025-05-29 22:54:00 -0300

    fix: move UseCustomFonts to correctly directory

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header/Header";
 import MainCard from "@/components/MainCard/MainCard";
 import TransactionList from "@/components/Transaction/TransactionList";
 import { colors } from "@/utils/colors";
-import loadCustomFonts from "@/components/hooks/UseCustomFonts";
+import loadCustomFonts from "@/hooks/UseCustomFonts";
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
