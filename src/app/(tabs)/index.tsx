594a97ce4b8c099827d83fcc24d8f5607f539f66
Jonatas <djonatas31@gmail.com>
2025-06-04 12:19:07 -0300

    refactor: change filename to index.tsx

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,10 @@
 import Header from "@/components/Header/Header";
 import MainCard from "@/components/MainCard/MainCard";
 import TransactionList from "@/components/Transaction/TransactionList";
-import UseCustomFonts from "@/hooks/UseCustomFonts";
 import { colors } from "@/utils/colors";
 import { StyleSheet, View } from "react-native";
 
-export default function HomeScreen() {
-  UseCustomFonts();
-
+export default function Index() {
   return (
     <View style={style.container}>
       <Header />
