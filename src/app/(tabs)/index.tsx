84ae15b147f273c7ee455ab78b70e576e3d59df3
Jonatas <djonatas31@gmail.com>
2025-06-05 09:44:50 -0300

    refactor: move MainCard

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header/Header";
-import MainCard from "@/components/MainCard/MainCard";
+import MainCard from "@/components/Cards/MainCard";
 import TransactionList from "@/components/Transaction/TransactionList";
 import { colors } from "@/utils/colors";
 import { StyleSheet, View } from "react-native";
